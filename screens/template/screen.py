9a0ecf1054839e506897dac65431ee1c96901bf9
karkak <karri@sipo.fi>
2019-05-27 12:00:29 +0300

    Python3

--- screens/template/screen.py
+++ screens/template/screen.py
@@ -1,6 +1,10 @@
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
 
-class DummyScreen(Screen):
+class BlackHole(object):
+    def __init__(self, **kw):
+        super(BlackHole, self).__init__()
+
+class DummyScreen(Screen, BlackHole):
     def __init__(self, **kwargs):
         super(DummyScreen, self).__init__(**kwargs)
